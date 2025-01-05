714d23e7d17bba1636a8bc392a8f24bd835325dc
Adeosun Abiola <abiolaolalekan39@gmail.com>
2025-01-05 15:19:07 +0100

    Worked on History, Council Structure and VisionMisssion Tabs on About Us dashboard route

--- src/components/Admin/AboutUsTabs.tsx
+++ src/components/Admin/AboutUsTabs.tsx
@@ -18,6 +18,12 @@ const AboutUsTabs = ({ aboutInfo, officials }: any) => {
   const [structureFormData, setStructureFormData] = useState<AboutUsType>(
     aboutInfo[1]
   );
+  const [visionFormData, setVisionFormData] = useState<AboutUsType>(
+    aboutInfo[2]
+  );
+  const [missionFormData, setMissionFormData] = useState<AboutUsType>(
+    aboutInfo[3]
+  );
   const [isSaving, setIsSaving] = useState(false);
 
   //   History Tab Data
@@ -143,6 +149,84 @@ const AboutUsTabs = ({ aboutInfo, officials }: any) => {
       }
     } catch (error) {
       toast.error("Error occurred while updating Council Structure info");
+      setIsSaving(false);
+      console.error("Error:", error as AxiosError);
+    }
+  };
+
+  //   Vision Tab Data
+  const handleVisionInputChange = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = event.target;
+    setVisionFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleVisionFormSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    console.log("Form Data:", visionFormData);
+
+    const submitData = new FormData();
+    submitData.append("title", visionFormData.title);
+    submitData.append("description", visionFormData.description);
+    if (visionFormData.image instanceof File) {
+      submitData.append("image", visionFormData.image);
+    }
+
+    try {
+      setIsSaving(true);
+      const response: AxiosResponse = await axios.put(`/about/3`, submitData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      if (response.data) {
+        toast.success("vision updated successfully!!!");
+        setIsSaving(false);
+        navigate("/dashboard/about-us");
+      }
+    } catch (error) {
+      toast.error("Error occurred while updating vision");
+      setIsSaving(false);
+      console.error("Error:", error as AxiosError);
+    }
+  };
+
+  //   Mission Tab Data
+  const handleMissionInputChange = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = event.target;
+    setMissionFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleMissionFormSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    console.log("Form Data:", missionFormData);
+
+    const submitData = new FormData();
+    submitData.append("title", missionFormData.title);
+    submitData.append("description", missionFormData.description);
+    if (missionFormData.image instanceof File) {
+      submitData.append("image", missionFormData.image);
+    }
+
+    try {
+      setIsSaving(true);
+      const response: AxiosResponse = await axios.put(`/about/4`, submitData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      if (response.data) {
+        toast.success("mission updated successfully!!!");
+        setIsSaving(false);
+        navigate("/dashboard/about-us");
+      }
+    } catch (error) {
+      toast.error("Error occurred while updating mission");
       setIsSaving(false);
       console.error("Error:", error as AxiosError);
     }
@@ -338,9 +422,10 @@ const AboutUsTabs = ({ aboutInfo, officials }: any) => {
           </form>
         </TabPanel>
         <TabPanel>
-          <form onSubmit={handleHistoryFormSubmit}>
-            <div className="grid grid-cols-[1fr_350px] gap-8">
+          <div className="grid grid-cols-2 gap-8">
+            <form onSubmit={handleVisionFormSubmit}>
               <div className="flex flex-col gap-4">
+                <h2 className="text-2xl font-bold text-blue-700">Our Vision</h2>
                 <div className="flex flex-col">
                   <label
                     htmlFor="title"
@@ -348,16 +433,14 @@ const AboutUsTabs = ({ aboutInfo, officials }: any) => {
                   >
                     Title
                   </label>
-                  <input
-                    id="title"
-                    type="text"
+                  <textarea
+                    id="text-area"
                     name="title"
-                    className="mt-2 p-2 border-[1px] border-[#D8DDE4] rounded-lg outline-none"
-                    placeholder="Enter Title"
-                    onChange={handleHistoryInputChange}
+                    className={`${styles.paragraph4} w-full h-[100px] max-h-[100px] p-5 border-[1px] mt-2 rounded-xl outline-none `}
+                    value={visionFormData.title}
+                    onChange={handleVisionInputChange}
                     required
-                    value={historyFormData.title}
-                  />
+                  ></textarea>
                 </div>
                 <div className="flex flex-col">
                   <label
@@ -369,55 +452,57 @@ const AboutUsTabs = ({ aboutInfo, officials }: any) => {
                   <textarea
                     id="text-area"
                     name="description"
-                    className={`${styles.paragraph4} w-full h-[250px] max-h-[250px] p-5 border-[1px] mt-2 rounded-xl outline-none `}
-                    value={historyFormData.description}
-                    onChange={handleHistoryInputChange}
+                    className={`${styles.paragraph4} w-full h-[200px] max-h-[200px] p-5 border-[1px] mt-2 rounded-xl outline-none `}
+                    value={visionFormData.description}
+                    onChange={handleVisionInputChange}
                     required
                   ></textarea>
                 </div>
               </div>
-              <div className="w-full h-[350px] bg-[#F4F7FC] my-5 rounded-md">
-                <input
-                  type="file"
-                  name="image"
-                  id="image"
-                  accept="image/jpeg, image/png"
-                  ref={historyImageInputRef}
-                  onChange={handleHistoryImageInputChange}
-                  className="hidden"
-                />
-                {historyImage ? (
-                  <div className="relative w-full h-full">
-                    <img
-                      src={historyImage}
-                      alt="Map of Kono District"
-                      className="h-full w-full object-contain"
-                    />
-                    <div className="absolute group hover:bg-black/30 w-full h-full top-0 left-0 grid place-items-center">
-                      <button
-                        type="button"
-                        className="bg-blue-700 hidden group-hover:block  text-white py-2 px-4 text-sm rounded-lg"
-                        onClick={handleHistoryImageSelector}
-                      >
-                        Change Image
-                      </button>
-                    </div>
-                  </div>
-                ) : (
-                  <div
-                    onClick={handleHistoryImageSelector}
-                    className="cursor-pointer w-full h-full rounded-md border-[1px] bg-[#F4F7FC] border-[#D8DDE4] flex items-center justify-center flex-col"
+              <Button name="Save Changes" py={4} px={8} />
+            </form>
+
+            <form onSubmit={handleMissionFormSubmit}>
+              <div className="flex flex-col gap-4">
+                <h2 className="text-2xl font-bold text-blue-700">
+                  Our Mission
+                </h2>
+                <div className="flex flex-col">
+                  <label
+                    htmlFor="title"
+                    className={`${styles.paragraph3} text-[#30354E]`}
                   >
-                    <img src={image} className="w-6 h-6" alt="Upload icon" />
-                    <p className={`${styles.paragraph4} text-[#849299] mt-1`}>
-                      Click to upload image
-                    </p>
-                  </div>
-                )}
+                    Title
+                  </label>
+                  <textarea
+                    id="text-area"
+                    name="title"
+                    className={`${styles.paragraph4} w-full h-[100px] max-h-[100px] p-5 border-[1px] mt-2 rounded-xl outline-none `}
+                    value={missionFormData.title}
+                    onChange={handleMissionInputChange}
+                    required
+                  ></textarea>
+                </div>
+                <div className="flex flex-col">
+                  <label
+                    htmlFor="description"
+                    className={`${styles.paragraph3} text-[#30354E]`}
+                  >
+                    Description
+                  </label>
+                  <textarea
+                    id="text-area"
+                    name="description"
+                    className={`${styles.paragraph4} w-full h-[200px] max-h-[200px] p-5 border-[1px] mt-2 rounded-xl outline-none `}
+                    value={missionFormData.description}
+                    onChange={handleMissionInputChange}
+                    required
+                  ></textarea>
+                </div>
               </div>
-            </div>
-            <Button name="Save Changes" py={4} px={8} />
-          </form>
+              <Button name="Save Changes" py={4} px={8} />
+            </form>
+          </div>
         </TabPanel>
         <TabPanel>
           {/* <form onSubmit={handleSubmit}>
